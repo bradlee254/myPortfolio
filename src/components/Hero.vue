3800d87a222f18ab057c6ce10b9746876b734b2a
bradle254 <leebrad341@gmail.com>
2025-12-30 14:45:14 +0300

    navebar

---
+++ src/components/Hero.vue
@@ -0,0 +1,36 @@
+
+<template>
+    <section id="home" class="min-h-screen flex items-center justify-center pt-16">
+        <div class="max-w-6xl mx-auto px-6 grid md:grid-cols-2 gap-12 items-center">
+            <div>
+                <p class="text-emerald-400 font-medium mb-3">
+                    Hi, I'm
+                </p> 
+                <h1 class="text-4xl md:text-6xl font-bold leading-tight mb-4">
+                    Bradlee Bashir <br /> <span class="text-emerald-400">Software Developer</span>
+                </h1>
+                <p class="text-zinc-400 max-w-lg mb-8">
+                    I build modern, scalable web applications using React, Vue,
+                    Svelte ,TypeScript, Node and modern JavaScript frameworks.
+                </p>
+                <div class="flex gap-4">
+                    <a href="#projects" class="px-6 py-3 rounded-lg bg-emerald-500 text black font-medium hover:bg-emerald-400 transition">
+                        View Projects
+                    </a>
+
+                    <a href="#contact" 
+                    class="px-6 py-3 rounded-lg border border-white/20 hover:border-white transition">
+                        Contact Me
+                    </a>
+                </div>
+            </div>
+
+            <!-- my photos -->
+             <div class="hidden md:flex justify-center">
+                <div class="w-72 h-72 rounded-full bg-gradient-to-br from-emerald-500/30 to-cyan-500/30 flex items-center justify-center">
+                    <span class="text-zinc-300">picture</span>
+                </div>
+             </div>
+        </div>
+    </section>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+    interface NavLinks {
+        label: string;
+        href: string;
+    }
+    const links: NavLinks[] = [
+        {label: 'Home', href: '#home'},
+        {label: 'About', href: '#about'},
+        {label: 'Projects', href: '#projects'},
+        {label: 'Contact', href: '#contact'},
+        {label: 'Skills', href: '#skills'},
+    ];
+</script>
+
+<template>
+    <header class=" fixed top-0 left-0 w-full z-50 bg-zinc-950/80 backdrop-blur border-b border-white/10">
+        <nav class="max-w-6xl mx-auto px-6 h-16 flex items-center justify-between">
+
+            <a href="#home" class="text-xl font-bold text-emerald-400">Bashir
+                <span class="text-white">.</span></a>
+
+                <!-- desktop links -->
+                 <ul class="hidden md:flex gap-8 text-sm">
+
+                    <li v-for="link in links" :key="link.href">
+                        <a :href="link.href" class="text-zinc-300 hover:text-white transition">
+                            {{ link.label }} </a>
+                    </li>
+                </ul>
+        </nav>
+    </header>
+</template>
