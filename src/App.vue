50586be932218bb96c17cfe4196090d262179207
bradle254 <leebrad341@gmail.com>
2025-12-30 14:11:13 +0300

    portforlio

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import Navbar from "./components/Navbar.vue"
+  import Home from "./layout/Home.vue"
+</script>
+
+<template>
+  <Navbar />
+  <Home />
+</template>
---
+++ src/layout/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+    import Hero from '../components/Hero.vue';
+    import About from '../components/About.vue';
+    import Projects from '../components/Projects.vue';
+    import Contact from '../components/Contacts.vue';
+    import Skills from '../components/Skills.vue';
+</script>
+
+<template>
+    <Hero />
+    <About />
+    <Skills />
+    <Projects />
+    <Contact />
+</template>
